58e2ed9f360b4d9c1a91b23e8388fa724b847749
hellogit <8530@qq.com>
2020-12-19 16:38:23 +0800

    B 第二次提交

--- src/main/java/com/atguigu/test/MyGitTest.java
+++ src/main/java/com/atguigu/test/MyGitTest.java
@@ -4,5 +4,6 @@ public class MyGitTest {
     public static void main(String[] args) {
         System.out.println("A 第一次输出");
         System.out.println("B 第一次输出");
+        System.out.println("B 第二次提交");
     }
 }
