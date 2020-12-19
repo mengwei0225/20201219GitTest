3d0e54d68cc860c562206db5dc5f3e56ace611b3
hellogit <8530@qq.com>
2020-12-19 16:36:32 +0800

    开发环境第一次提交

--- src/main/java/com/atguigu/test/MyGitTest.java
+++ src/main/java/com/atguigu/test/MyGitTest.java
@@ -4,5 +4,7 @@ public class MyGitTest {
     public static void main(String[] args) {
         System.out.println("A 第一次输出");
         System.out.println("B 第一次输出");
+        System.out.println("开发环境，A第一次更改");
+        System.out.println("生产环境，A第一次更改");
     }
 }
